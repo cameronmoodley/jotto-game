4bab3eefde54df6ce1b3593ad7370a43e10e639a
cameronmoodley <moodley.cameron@gmail.com>
2020-06-29 10:29:53 +0200

    added in redux tests

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../../helpers/';
-import { CORRECT_GUESS, GUESS_WORD } from './actionTypes';
+import { CORRECT_GUESS, GUESS_WORD, SET_SECRET_WORD } from './actionTypes';
 
 export const guessWord = (guessedWord) => {
 	return (dispatch, getState) => {
@@ -15,5 +16,16 @@ export const guessWord = (guessedWord) => {
 		});
 
 		guessedWord === secretWord && dispatch({ type: CORRECT_GUESS });
+	};
+};
+
+export const getSecretWord = () => {
+	return (dispatch) => {
+		return axios.get('http://localhost:3030/').then((response) => {
+			dispatch({
+				type: SET_SECRET_WORD,
+				payload: response.data,
+			});
+		});
 	};
 };
--- src/redux/reducers/secretWordReducer.js
+++ src/redux/reducers/secretWordReducer.js
@@ -1,3 +1,12 @@
-export default (state = null, action) => {
+import { SET_SECRET_WORD } from '../actions/actionTypes';
+
+export default (state = null, { type, payload }) => {
+	switch (type) {
+		case SET_SECRET_WORD:
+			return payload;
+
+		default:
+			return state;
+	}
 	return state;
 };
